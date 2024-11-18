5b8cfd64a1180a4ee23584090488b1508e561168
Avijit Paul <avijit.odcl@gmail.com>
2024-11-18 14:18:25 +0600

    Basic Book Management CRUD

--- src/main/java/com/example/crud/service/BookService.java
+++ src/main/java/com/example/crud/service/BookService.java
@@ -27,4 +27,13 @@ public class BookService {
     public Optional<Book> getBook (Long id){
         return bookRepository.findById(id);
     }
+
+    public String deleteBook(Long id){
+        if (bookRepository.existsById(id)){
+            bookRepository.deleteById(id);
+            return "Book deleted. Id: "+id;
+        }else {
+            return "Book Not Found";
+        }
+    }
 }
